e8626eef08ad84387285b7101062e074793c960b
mojith <gayan.u.mojith@gmail.com>
2017-03-30 23:05:03 +0530

    histogram program is completed

--- VerificationPrograms/Histogram/text.c
+++ VerificationPrograms/Histogram/text.c
@@ -6,6 +6,8 @@ int main(){
 	int temp;
 	int address = 0x10350;
 	int i,min=20000,max=0,median=0,sum=0, average=0;
+	int his0 = 0, his1 = 0, his2 = 0, his3 = 0, his4 = 0;
+	int his5 = 0, his6 = 0, his7 = 0, his8 = 0, his9 = 0;
 	//temp *p;
 	//temp = *(int*)address;
 	//address+=0x04;
@@ -24,10 +26,60 @@ int main(){
 		if(i = 500){
 			median = temp;
 		}
+
+		if(0<=i && i<100){
+			his0++;
+		}
+		if(100<=i && i<200){
+			his1++;
+		}
+		if(200<=i && i<300){
+			his2++;
+		}
+		if(300<=i && i<400){
+			his3++;
+		}
+		if(400<=i && i<500){
+			his4++;
+		}
+		if(500<=i && i<600){
+			his5++;
+		}
+		if(600<=i && i<700){
+			his6++;
+		}
+		if(700<=i && i<800){
+			his7++;
+		}
+		if(800<=i && i<900){
+			his8++;
+		}
+		if(900<=i && i<=1000){
+			his9++;
+		}
+
 		address+=0x04;
 	}
 	average = sum/1000;
+	
 
+	*(int*)0x10150 = min;
+	*(int*)0x10150 = max;
+	*(int*)0x10150 = median;
+	*(int*)0x10150 = average;
+
+	*(int*)0x10150 = his0;
+	*(int*)0x10150 = his1;
+	*(int*)0x10150 = his2;
+	*(int*)0x10150 = his3;
+	*(int*)0x10150 = his4;
+	*(int*)0x10150 = his5;
+	*(int*)0x10150 = his6;
+	*(int*)0x10150 = his7;
+	*(int*)0x10150 = his8;
+	*(int*)0x10150 = his9;
+
+	
 	while(1){}
 	return 0;
 }
