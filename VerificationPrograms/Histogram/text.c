b538f54e8c89003510e99f8fc73a02f068f1c3e6
mojith <gayan.u.mojith@gmail.com>
2017-03-30 22:24:51 +0530

    Initial parts of the histogram program is added

--- VerificationPrograms/Histogram/text.c
+++ VerificationPrograms/Histogram/text.c
@@ -4,8 +4,29 @@ int main(){
 
 	//int *p = (int *) u;
 	int temp;
+	int address = 0x10350;
+	int i,min=20000,max=0,median=0,sum=0, average=0;
 	//temp *p;
-	temp = *(int*)0x10150;
+	//temp = *(int*)address;
+	//address+=0x04;
+	//temp = *(int*)address;
+
+	for(i=0;i<1000;i++){
+		temp = *(int*)address;
+		sum+=temp;
+		if(temp < min){
+			min = temp;
+		}
+
+		if(temp > max){
+			max = temp;
+		}
+		if(i = 500){
+			median = temp;
+		}
+		address+=0x04;
+	}
+	average = sum/1000;
 
 	while(1){}
 	return 0;
