9c616233f2574180dcc24e1211f637106edd31c1
mojith <gayan.u.mojith@gmail.com>
2017-04-02 16:32:16 +0530

    Two new LargeConvolution and SortLarge programs are added

--- VerificationPrograms/Histogram/hist.c
+++ VerificationPrograms/Histogram/hist.c
@@ -39,19 +39,31 @@ int main(){
 
 
 	address = 0x10370;
-	int his0 = 0, his1 = 0;
+	int his0 = 0, his1 = 0, his2 = 0, his3 = 0, his4 = 0;
 	for(i=0;i<100;i++){
 		temp = *(int*)address;
-		if(0<=temp && temp<5000){
+		if(0<=temp && temp<2000){
 			his0++;
 		}
-		if(5000<=temp && temp<10000){
+		if(2000<=temp && temp<4000){
 			his1++;
+		}
+		if(4000<=temp && temp<6000){
+			his2++;
+		}
+		if(6000<=temp && temp<8000){
+			his3++;
+		}
+		if(8000<=temp && temp<5000){
+			his4++;
 		}
 		address+=0x04;
 	}
 	*(int*)0x11FF8 = his0;
 	*(int*)0x11FF8 = his1;
+	*(int*)0x11FF8 = his2;
+	*(int*)0x11FF8 = his3;
+	*(int*)0x11FF8 = his4;
 
 	while(1){}
 return 0;
